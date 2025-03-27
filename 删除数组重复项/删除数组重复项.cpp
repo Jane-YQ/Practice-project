c20ebb6a17bdeb1ba89e8c559022dd61109246ed
Jane-YQ <869944365@qq.com>
2025-03-27 11:27:21 +0800

    删除数组重复元素-RemoveDuplicates
    把非严格递增排列的数组 nums 中重复出现的元素删除，使每个元素只出现一次 ，且元素的相对顺序保持一致。返回删除后数组的元素个数，和数组的新长度。

---
+++ 删除数组重复项/删除数组重复项.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		int length = nums.size();
+		if (length == 0) {
+			return 0;
+		}
+
+		int a = 1;
+		for (int i = 1; i < length; i++) {
+			if (nums[i] != nums[i - 1]) {
+				nums[a] = nums[i];
+				a++;
+			}
+		}
+		return a;
+	}
+};
+
+int main() {
+	vector<int> nums = { 0,0,1,1,1,2,2,3,3,4 };
+	Solution sol;
+	int newLength = sol.removeDuplicates(nums);
+	cout << "新数组长度: " << newLength << endl;
+	cout << "修改后的数组: ";
+	for (int i = 0; i < newLength; i++) {
+		cout << nums[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
