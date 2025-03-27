2246d58e9ffcd82dee881d8eaa58a5c45379fe35
Jane-YQ <869944365@qq.com>
2025-03-27 12:28:44 +0800

    数组中的众数-MajorityElement
    给定一个大小为 n 的数组 nums ，返回其中出现次数 大于 ⌊ n/2 ⌋ 的元素。

---
+++ 数组中的众数/数组中的众数.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int count = 0;// 计数器，记录当前候选元素的数量
+		int a = 0; // 记录候选元素的值
+		int i = 0;
+		for (int num : nums) {
+
+			// 如果计数器为0，表示当前没有候选元素
+		    // 将当前元素设为新的候选元素
+			if (count == 0) {
+				a = num;
+				count++;
+			}
+
+			// 如果计数器不为0且当前元素等于候选元素
+			// 说明又遇到一个相同的元素，增加计数
+			else if (count != 0 && a == num) {
+				count++;
+			}
+
+			// 如果计数器不为0且当前元素不等于候选元素
+			// 说明遇到不同元素，减少计数（抵消）
+			else {
+				count--;
+			}
+		}
+		return a;// 返回最终的候选元素
+	}
+};
+
+int main() {
+	Solution sol;
+	vector<int> nums = { 1,2,2,3,0,3,2,4 };
+	cout << "数组中最多的元素：" << sol.majorityElement(nums) << endl;
+	return 0;
+}
