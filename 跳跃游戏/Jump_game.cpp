e42a719b19b5ef3e90077b1b9dd147c6e0c78d5f
Jane-YQ <869944365@qq.com>
2025-03-27 11:31:31 +0800

    跳跃游戏-canJump
    给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。

---
+++ 跳跃游戏/Jump_game.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool jumpgame(vector<int>& nums) {
+		int mx = 0;
+		int length = nums.size();
+		for (int i = 0; i < length; i++) {
+			if (i > mx) {
+				return false;
+			}
+			mx = max(mx, i + nums[i]);
+
+		}
+		return true;
+	}
+};
+
+int main() {
+	vector<int> nums = { 1,3,3,0,2,1,2 };
+	Solution sol;
+	bool result = sol.jumpgame(nums);
+	cout << "能否到达终点: " << (result ? "可以" : "不可以") << endl;
+	cout << "数组内容: ";
+	for (int num : nums) {
+		cout << num << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
