b9c575d07832a08d9d17241cd85ee4c6783761a7
Jane-YQ <869944365@qq.com>
2025-03-27 10:55:31 +0800

    RemoveElement-移除元素
    通过一个数组 nums 和一个值 val，原地移除数组中所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。

---
+++ 快慢指针/快慢指针.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		int n = nums.size();
+		int k = 0;
+		for (int i = 0; i < n; i++) {
+			if (nums[i] != val) {
+				nums[k] = nums[i];
+				k++;
+			}
+		}
+		return k;
+	}
+};
+
+int main() {
+	vector<int> nums = { 3, 2, 2, 3 };
+	int val = 3;
+	Solution sol;
+	int newLength = sol.removeElement(nums, val);
+	cout << "新数组长度: " << newLength << endl;
+	cout << "修改后的数组: ";
+	for (int i = 0; i < newLength; i++) {
+		cout << nums[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
